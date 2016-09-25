1700acd76d405dadb14d2297a3a328e980a62532
erika1066 <hastings.erika@gmail.com>
2016-09-25 23:33:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,17 @@
+//var array = [
+//  "I am" + i +"strange loop."
+//]
+
+function forLoop(array){
+  //const strangeArray = forloop(array);
+for let(i = 0; i < 25; i++)}
+
+forLoop(array){
+if(i = 1){
+  console.log("I am" + i + "strange loop.")
+    }
+  else {
+    console.log("I am" + i + "strange loops.");
+    }
+
+}
